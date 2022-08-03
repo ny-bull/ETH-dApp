fb4ae752ee345afbf62853ad9b7fd0c23c9d1025
nyuta <nyuta.main@gmail.com>
2022-08-03 23:23:45 +0000

    ✨ feat - payable

--- my-wave-portal/scripts/run.js
+++ my-wave-portal/scripts/run.js
@@ -1,34 +1,53 @@
+// run.js
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
-    const wavePortal = await waveContract.deployed();
-  
-    console.log("Contract deployed to:", wavePortal.address);
-    console.log("Contract deployed by:", owner.address);
-  
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
-  
-    let waveTxn = await waveContract.wave();
-    await waveTxn.wait();
-  
-    waveCount = await waveContract.getTotalWaves();
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  /*
+   * 0.1ETHをコントラクトに提供してデプロイする
+   */
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  await waveContract.deployed();
+  console.log("Contract deployed to:", waveContract.address);
 
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    await waveTxn.wait();
-  
-    waveCount = await waveContract.getTotalWaves();
+  /*
+   * コントラクトの残高を取得し、結果を出力（0.1ETHであることを確認）
+   */
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  /*
+   * Waveし、トランザクションが完了するまで待機
+   */
+  let waveTxn = await waveContract.wave("A message!");
+  await waveTxn.wait();
+
+  /*
+   * Waveした後のコントラクトの残高を取得し、結果を出力（0.0001ETH引かれていることを確認）
+   */
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
-  
+
 const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
 runMain();
